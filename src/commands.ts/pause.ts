8383de6aba5f2687af57ae487fb5cdbb626ef2fb
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-04-18 21:47:50 +0530

    ✨ Add pause/play/resume

--- src/commands.ts/pause.ts
+++ src/commands.ts/pause.ts
@@ -1,13 +1,14 @@
-import { Message } from 'discord.js';
-import { playRequest } from '../controller';
+import { connectionMap } from '../controller';
 import { ERROR_MESSAGES } from '../shared/constants';
-export let connection;
-export const playCommand = (message: Message) => {
+
+export const pauseCommand = message => {
   if (!message.member.voice.channel) return message.reply(ERROR_MESSAGES.USER_NOT_IN_A_VOICE_CHANNEL);
+  if (!message.guild.me.voice.channel) return message.reply(ERROR_MESSAGES.BOT_NOT_IN_A_VOICE_CHANNEL);
   if (!message.member.voice.channel) return message.reply(ERROR_MESSAGES.USER_NOT_IN_A_VOICE_CHANNEL);
   if (message.guild.me.voice.channel) {
     if (!(message.member.voice.channelID === message.guild.me.voice.channelID))
       return message.reply(ERROR_MESSAGES.USER_NOT_IN_THE_SAME_CHANNEL);
   }
-  playRequest(message);
+  connectionMap.get(message.guild.id).dispatcher.pause(true);
+  message.channel.send('Music paused!');
 };
