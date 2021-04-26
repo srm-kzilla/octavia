afdd60584c86e9a93cfed4ac9f34858eec6f4af0
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-04-27 03:15:21 +0530

    🐛 Fix auth bug

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -3,7 +3,8 @@ import { userInVoiceChannelCheck } from '../shared/auth';
 import { MESSAGES } from '../shared/constants';
 
 export const leaveCommand = (message: Message) => {
-  if (userInVoiceChannelCheck) {
+  if (userInVoiceChannelCheck(message)) {
+    console.log('10000');
     message.guild.me.voice.channel.leave();
     message.reply(MESSAGES.LEAVE_CHANNEL);
   }
