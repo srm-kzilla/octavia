9f6d57b359f2b385b6645b3278a888cb69e596cb
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-04-27 03:36:50 +0530

    🥅 Handle errors in lyrics.ts

--- src/commands/lyrics.ts
+++ src/commands/lyrics.ts
@@ -1,17 +1,25 @@
 import { getLyrics } from 'genius-lyrics-api';
 import config from '../config';
 import { connectionMap } from '../controller';
-import { EMBED } from '../shared/constants';
+import LoggerInstance from '../loaders/logger';
+import { EMBED, ERROR_MESSAGES, randomNumber } from '../shared/constants';
 
 export const lyricsCommand = async message => {
-  const options = {
-    apiKey: config.GENIUS_API_KEY,
-    title: `${
-      connectionMap.get(message.guild.id).queue[connectionMap.get(message.guild.id).currentSong].originalTitle
-    }`,
-    artist: `${connectionMap.get(message.guild.id).queue[connectionMap.get(message.guild.id).currentSong].artistName}`,
-    optimizeQuery: true,
-  };
-  const lyrics = await getLyrics(options);
-  message.channel.send(EMBED().setThumbnail('').setDescription(lyrics.substring(0, 2047)));
+  try {
+    const options = {
+      apiKey: config.GENIUS_API_KEY,
+      title: `${
+        connectionMap.get(message.guild.id).queue[connectionMap.get(message.guild.id).currentSong].originalTitle
+      }`,
+      artist: `${
+        connectionMap.get(message.guild.id).queue[connectionMap.get(message.guild.id).currentSong].artistName
+      }`,
+      optimizeQuery: true,
+    };
+    const lyrics = await getLyrics(options);
+    message.channel.send(EMBED().setThumbnail('').setDescription(lyrics.substring(0, 2047)));
+  } catch (error) {
+    LoggerInstance.error(error.message);
+    message.channel.send(ERROR_MESSAGES.UNKNOWN_ERROR[randomNumber(ERROR_MESSAGES.UNKNOWN_ERROR.length)]);
+  }
 };
