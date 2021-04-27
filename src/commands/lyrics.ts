699fef7fc26afc201b80985d9bf93236cd0caf0e
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-04-27 22:14:01 +0530

    ♻️ Refactor lyrics module

--- src/commands/lyrics.ts
+++ src/commands/lyrics.ts
@@ -1,24 +1,18 @@
-import { getLyrics } from 'genius-lyrics-api';
-import config from '../config';
 import { connectionMap } from '../controller';
 import LoggerInstance from '../loaders/logger';
 import { EMBED, ERROR_MESSAGES, randomNumber } from '../shared/constants';
+import { lyricsHelper } from '../shared/lyrics';
 
 export const lyricsCommand = async message => {
   try {
-    const options = {
-      apiKey: config.GENIUS_API_KEY,
-      title: `${
-        connectionMap.get(message.guild.id).queue[connectionMap.get(message.guild.id).currentSong].originalTitle
-      }`,
-      artist: `${
-        connectionMap.get(message.guild.id).queue[connectionMap.get(message.guild.id).currentSong].artistName
-      }`,
-      optimizeQuery: true,
-    };
-    const lyrics = await getLyrics(options);
+    const lyrics = await lyricsHelper(
+      `${connectionMap.get(message.guild.id).queue[connectionMap.get(message.guild.id).currentSong].originalTitle}`,
+      `${connectionMap.get(message.guild.id).queue[connectionMap.get(message.guild.id).currentSong].artistName}`,
+    );
+
     if (!lyrics) return message.channel.send(ERROR_MESSAGES.UNABLE_TO_FIND_LYRICS);
-    message.channel.send(EMBED().setThumbnail('').setDescription(lyrics.substring(0, 2047)));
+    if (lyrics.length > 2047) message.channel.send(EMBED().setThumbnail('').setDescription(lyrics.substring(0, 2047)));
+    else message.channel.send(EMBED().setThumbnail('').setDescription(lyrics));
   } catch (error) {
     LoggerInstance.error(error.message);
     message.channel.send(ERROR_MESSAGES.UNKNOWN_ERROR[randomNumber(ERROR_MESSAGES.UNKNOWN_ERROR.length)]);
